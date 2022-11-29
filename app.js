c516d6fd040cb5c661f78bb4e3fbf9ce299b76a5
abdulmalik-yahya <dolphin.tc.ye@gmail.com>
2022-11-30 00:20:13 +0300

    Add Procfile, .gitignore and update ports

--- app.js
+++ app.js
@@ -154,6 +154,11 @@ app.get("/:customListName", (req, res) => {
 });
 
 
-app.listen(3000, () => {
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
     console.log("Server started on port 3000");
 });
